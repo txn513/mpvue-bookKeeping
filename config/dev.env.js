35b5d5a8c4875e035ab7cd77cc066a0ffc506a7a
Lance Tian <xtian513@gmail.com>
2019-06-28 19:11:44 +0800

    修改dev

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // API_ROOT: '"http://192.168.31.133:5000"'
-  API_ROOT: '"https://bkdemo.juheworld.cn"'
+  API_ROOT: '"http://192.168.31.133:5000"'
+  // API_ROOT: '"https://bkdemo.juheworld.cn"'
 })
